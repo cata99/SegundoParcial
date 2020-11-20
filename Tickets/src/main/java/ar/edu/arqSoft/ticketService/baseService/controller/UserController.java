ffb595bc3f8e176ec035b9e401c3f691675c2d94
cata99 <48757813+cata99@users.noreply.github.com>
2020-11-19 21:54:33 -0300

    Add structure controllers

---
+++ Tickets/src/main/java/ar/edu/arqSoft/ticketService/baseService/controller/UserController.java
@@ -0,0 +1,23 @@
+package ar.edu.arqSoft.ticketService.baseService.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ar.edu.arqSoft.ticketService.baseService.services.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController{
+	
+	@Autowired
+	private UserService userService;
+	
+	
+}
