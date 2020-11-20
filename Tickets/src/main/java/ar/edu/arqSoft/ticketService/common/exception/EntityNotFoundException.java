766a15fb15d3442431b92ca2bf109ea08a312b17
cata99 <48757813+cata99@users.noreply.github.com>
2020-11-20 16:40:40 -0300

    Add config

---
+++ Tickets/src/main/java/ar/edu/arqSoft/ticketService/common/exception/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package ar.edu.arqSoft.ticketService.common.exception;
+
+
+public class EntityNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
