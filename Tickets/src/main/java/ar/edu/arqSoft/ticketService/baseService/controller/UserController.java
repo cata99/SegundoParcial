91ca0bd1fc44671391a1a81db2c55d8721eed8a8
AngeloMenel98 <angelomenel@hotmail.com>
2020-11-20 16:26:18 -0300

    getAll usersController

--- Tickets/src/main/java/ar/edu/arqSoft/ticketService/baseService/controller/UserController.java
+++ Tickets/src/main/java/ar/edu/arqSoft/ticketService/baseService/controller/UserController.java
@@ -2,6 +2,7 @@ package ar.edu.arqSoft.ticketService.baseService.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
+import ar.edu.arqSoft.ticketService.baseService.dto.UserResponseDto;
 import ar.edu.arqSoft.ticketService.baseService.services.UserService;
 
 @Controller
@@ -19,5 +23,16 @@ public class UserController{
 	@Autowired
 	private UserService userService;
 	
-	
+	 @SuppressWarnings("unchecked")
+	 @RequestMapping(method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+	 @ResponseStatus(code= HttpStatus.CREATED)
+	 public @ResponseBody List<UserResponseDto> getAll(){
+			try {
+				UserResponseDto dto =(UserResponseDto) userService.;		
+				return (List<UserResponseDto>) dto;
+			} catch (EntityNotFoundException e) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Film Not Found", e);
+			} catch (BadRequest) { 
+				
+			}
 }
