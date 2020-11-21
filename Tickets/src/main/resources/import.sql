867a7b05d29e1f1605ed1d64320ac2dfc2a1866d
AngeloMenel98 <angelomenel@hotmail.com>
2020-11-21 20:00:31 -0300

    Changes in import.sql

--- Tickets/src/main/resources/import.sql
+++ Tickets/src/main/resources/import.sql
@@ -1,3 +1,10 @@
-insert into State (idState,name, task) values (1,"Created",1)
-insert into State (idState,name, task) values (1,"In progress",2)
-insert into State (idState,name, task) values (1,"Finish",1)
+insert into PROYECT (name,description,startDate,finishDate, state) values ("Proyect1", "example", "11-01-2002", "11-31-2002", 1)
+
+
+insert into TASK (name,description, proyect, users, state) values ("Task1","example", 1 , 1, 1)
+
+
+
+insert into STATE (name) values ("Created")
+insert into STATE (name) values ("In progress",2)
+insert into STATE (name) values ("Finish")
